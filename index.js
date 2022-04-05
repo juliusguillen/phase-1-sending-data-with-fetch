58b7f300a97a27d8d7d78008414a20a11571c454
christian julius guillen <juliusguillen@gmail.com>
2022-04-05 09:44:53 -0500

    commit tree d08a96047f33f317d372a2e54ef8c4ad60b6d762

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+function submitData(name, email) {
+    const configuration = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({name, email}),
+};
+return fetch("http://localhost:3000/users", configuration)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function(object) {  
+      //{ id: 249, name: 'Steve', email: 'steve@steve.com' }
+    
+    document.body.innerHTML = object['id']
+  })
+  .catch(function (response){
+ document.body.innerHTML = response
+  
+})
+}
+//so i have to be able to grab that id and apend it to the dom 
